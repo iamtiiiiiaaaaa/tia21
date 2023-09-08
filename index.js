1bca8adab98bccd139c1525c5ea0c6ebd9fc8543
@code <jj4068851@gmail.com>
2023-09-08 07:41:11 +0700

    Add: initial second page

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const firstPage = document.getElementById("first-page");
+const secondPage = document.getElementById("second-page");
 const kadoImg = document.getElementById("kado-img");
-kadoImg.addEventListener("click", () => firstPage.classList.add("d-none"));
+kadoImg.addEventListener("click", () => {
+    firstPage.classList.add("d-none");
+    secondPage.classList.remove("d-none");
+    document.body.style.backgroundColor = "#F4C2D7";
+});
