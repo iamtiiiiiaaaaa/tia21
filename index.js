72f26523bc6a1076d8d2dc6d4976c7cb5892e254
@code <jj4068851@gmail.com>
2023-09-11 07:15:03 +0700

    Add: add more guess

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const firstPage = document.getElementById("first-page");
 const secondPage = document.getElementById("second-page");
 const kadoImg = document.getElementById("kado-img");
-const dot = document.createElement("div");
+const dot0 = document.createElement("div");
+const dot1 = document.createElement("div");
+const dot2 = document.createElement("div");
+const dot3 = document.createElement("div");
+
 kadoImg.addEventListener("click", () => {
     firstPage.classList.add("d-none");
     secondPage.classList.remove("d-none");
@@ -18,8 +22,14 @@ const btnNext1 = document.getElementById("next-1");
 btnNext1.addEventListener("click", () => {
     document.getElementById("statement1").classList.add("d-none");
     document.getElementById("statement2").classList.remove("d-none");
-    dot.classList.add("dot");
-    secondPage.appendChild(dot);
+    dot0.classList.add("dot0");
+    dot1.classList.add("dot1");
+    dot2.classList.add("dot2");
+    dot3.classList.add("dot3");
+    secondPage.appendChild(dot0);
+    secondPage.appendChild(dot1);
+    secondPage.appendChild(dot2);
+    secondPage.appendChild(dot3);
 })
 
 const btnNext2 = document.getElementById("next-2");
@@ -29,10 +39,29 @@ btnNext2.addEventListener("click", () => {
     setInterval(() => navigator.vibrate(1000), 1000);
 })
 
-dot.addEventListener("click", () => {
+dot0.addEventListener("click", () => {
     document.getElementById("statement2").classList.add("d-none");
     document.getElementById("statement3").classList.remove("d-none")
-    dot.setAttribute("class", "d-none");
+    dot0.setAttribute("class", "d-none");
+    dot1.setAttribute("class", "d-none");
+    dot2.setAttribute("class", "d-none");
+    dot3.setAttribute("class", "d-none");
+
+})
+dot1.addEventListener("click", () => {
+    secondPage.classList.add("d-none");
+    document.getElementById("error-pages").classList.remove("d-none");
+    setInterval(() => navigator.vibrate(1000), 1000);
+})
+dot2.addEventListener("click", () => {
+    secondPage.classList.add("d-none");
+    document.getElementById("error-pages").classList.remove("d-none");
+    setInterval(() => navigator.vibrate(1000), 1000);
+})
+dot3.addEventListener("click", () => {
+    secondPage.classList.add("d-none");
+    document.getElementById("error-pages").classList.remove("d-none");
+    setInterval(() => navigator.vibrate(1000), 1000);
 })
 
 const btnNext3 = document.getElementById("next-3");
